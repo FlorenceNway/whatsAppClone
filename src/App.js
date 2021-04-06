73d84668f6b02c8bf481826cfb5e33d65dc03049
Florence <florence.nway@hotmail.com>
2021-04-06 02:05:01 +0100

    chat component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import Chat from './Components/Chat';
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-      {/* Sidebar */}
       <Sidebar/>
-      {/* Chat */}
+      <Chat />
       </div>
     </div>
   );
