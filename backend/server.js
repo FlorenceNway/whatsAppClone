c42b53cb97547ec8b80ea4aeff1e461920a10a80
Florence <florence.nway@hotmail.com>
2021-04-11 00:26:46 +0100

    mongoose connect url

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 //importing ( in package.json - added type: module - which allows us to use ES6 feture ( instead of const module = require('module')))
 import express from 'express';
+import mongoose from 'mongoose';
 
 //app config
 const app = express()
@@ -8,6 +9,13 @@ const port = process.env.PORT || 9000
 // middleware
 
 // DB config
+
+const connection_url = 'mongodb+srv://admin:1Mip2rbJGWSMAUro@cluster0.883rx.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+mongoose.connect(connection_url,{
+    userCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // api routes
 app.get('/', (req,res) => res.status(200).send('hello world'))
