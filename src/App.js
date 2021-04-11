51d96a4d599840d257579d01f77711ed0fddc353
Florence <florence.nway@hotmail.com>
2021-04-11 15:44:22 +0100

    connect pusher in FE

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import Sidebar from './Components/Sidebar';
 import Chat from './Components/Chat';
+import { useEffect } from 'react';
+import Pusher from 'pusher-js';
 
 function App() {
+  useEffect(() => {
+    var pusher = new Pusher('<key>', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('messages'); // use pusher channel name -messages
+    channel.bind('inserted', function(data) { // event name - insert
+      alert(JSON.stringify(data));
+    });
+  },[])
+
   return (
     <div className="app">
       <div className="app__body">
