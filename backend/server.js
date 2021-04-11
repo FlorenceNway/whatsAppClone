e0d915f6faaf2a70d33755760f8f770e301b5fbc
Florence <florence.nway@hotmail.com>
2021-04-11 15:04:54 +0100

    create change stream

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,56 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import Messages from './dbMessages.js';
+import Pusher from 'pusher';
 
 //app config
 const app = express()
 const port = process.env.PORT || 9000
 
+const pusher = new Pusher({
+    appId: "1186361",
+    key: "fd4103a6438ae7bf1733",
+    secret: "d804685ffa047f48c826",
+    cluster: "eu",
+    useTLS: true
+  });
+
+
 // middleware
 app.use(express.json())
 
 // DB config
-const connection_url = 'mongodb+srv://admin:nbhnPNApPVPOAkw4@cluster0.883rx.mongodb.net/whatsappdb?retryWrites=true&w=majority'
+const connection_url = 'mongodb+srv://admin:<pw>@cluster0.883rx.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 mongoose.connect(connection_url,{
     userCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+    // create change stream that should connect to pusher
+const db = mongoose.connection
+db.once('open',() => {
+    console.log("DB connected")
+    // give the name what collection to listen
+    const msgCollection = db.collection("messagecontents"); // !! collection name same as collection from schema and cluster
+    const changeStream = msgCollection.watch() // watch that collection changes
+
+    // function to fire when  somthing change in our DB
+    changeStream.on('change',(change) => { // what change in DB = input that made by FE
+        console.log(change)
+        if(change.operationType === 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted', {
+                name: messageDetails.user,
+                message: messageDetails.message
+            });
+        }
+        else {
+            console.log('Error triggering Pusher')
+        }
+    })
+}
+
+)
 
 // api routes
 app.get('/', (req,res) => res.status(200).send('hello world'))
@@ -29,6 +64,18 @@ app.post('/messages/new',(req,res) => {
             res.status(500).send(err)
         }else {
             res.status(201).send(`new message created: \n ${data}`)
+        }
+    })
+})
+
+// get all messages from DB
+app.get('/messages/sync',(req,res) => {
+    // eslint-disable-next-line array-callback-return
+    Messages.find((err,data) => {
+        if(err) {
+            res.status(500).send(err)
+        }else {
+            res.status(200).send(`new message created: \n ${data}`)
         }
     })
 })
