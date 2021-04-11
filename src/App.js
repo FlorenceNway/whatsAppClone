c3df80eb067e81304a2b7f56d332c94a69ab8361
Florence <florence.nway@hotmail.com>
2021-04-12 00:02:05 +0100

    show all initial messages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Sidebar from './Components/Sidebar';
 import Chat from './Components/Chat';
-import { useEffect } from 'react';
 import Pusher from 'pusher-js';
+import axios from './axios.js';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+// to fetch all initial inoformation
   useEffect(() => {
-    var pusher = new Pusher('<key>', {
+    axios
+    .get('./messages/sync')
+    .then(response => {
+      setMessages(response.data)
+    })
+  },[])
+
+  useEffect(() => {
+    var pusher = new Pusher('fd4103a6438ae7bf1733', {
       cluster: 'eu'
     });
 
     var channel = pusher.subscribe('messages'); // use pusher channel name -messages
-    channel.bind('inserted', function(data) { // event name - insert
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function(newMessage) { // event name - insert
+      setMessages([...messages, newMessage])
     });
-  },[])
+
+    // even messages change, to make sure one subscriber at a time to listen (if huge group chat, it can be many messages to listen when multi users send)
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+
+  },[messages])
 
   return (
     <div className="app">
       <div className="app__body">
       <Sidebar/>
-      <Chat />
+      <Chat messages={messages}/>
       </div>
     </div>
   );
