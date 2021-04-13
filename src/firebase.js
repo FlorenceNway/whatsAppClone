7e6942d45eabf914d6cc57862cea235589a7c5d8
Florence <florence.nway@hotmail.com>
2021-04-13 23:32:58 +0100

    firebase auth and db set up

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAWg0Hp9F1GZgpthr_dNAi4yzYDXeA6V38",
@@ -8,3 +9,15 @@ const firebaseConfig = {
     appId: "1:400092445226:web:d310038dabb77d1d9ce153",
     measurementId: "G-4VG304JG54"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  // to access firestore instance , basically getting our database
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  //for google authentication
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth, provider};
+  export default db;
